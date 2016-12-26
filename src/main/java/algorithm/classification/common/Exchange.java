eb9cbb2ca15861eebbbefe484f92989ed193ba56
gangwen.xu <gangwen.xu@qunar.com>
2016-12-26 18:13:39 +0800

    添加swap交换函数

--- src/main/java/algorithm/classification/common/Exchange.java
+++ src/main/java/algorithm/classification/common/Exchange.java
@@ -1,15 +1,17 @@
 package algorithm.classification.common;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-
 /**
  * Created by bbking on 16-12-25.
  */
 public class Exchange {
 
-    public static void swap(Object [] arr,int i, int j) {
+    /**
+     * 交换函数
+     * @param arr
+     * @param i
+     * @param j
+     */
+    public static void swap(Object[] arr, int i, int j) {
         Object temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
